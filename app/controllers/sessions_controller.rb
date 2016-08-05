59aab0f12b107744b443e1e385762b7bf24d93f4
Eric Booker & Justin Bryeans & Max Bigras & Sean Lee & Shawn Tuttle <pair+ericbooker12.justinbryeans.mbigras.seanyboy49.shawnte@devbootcamp.com>
2016-08-05 12:29:07 -0700

    create register and login partial for layout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+# login
+get '/login' do
+end
+
+post '/login' do
+end
+
+# logout
+get '/logout' do
+end
+
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+# display all users
+get '/users' do
+end
+
+#  form for creating new users
+get '/users/new' do
+end
+
+# create a new user
+post '/users' do
+  if params[:password] == params[:password2]
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      @errors
+      erb :index
+    end
+  else
+    @errors
+    erb :index
+  end
+end
+
+# display specific user
+get '/users/:id' do
+end
+
+# form for editing a user
+get 'users/:id/edit' do
+end
+
+# update a specific user
+put '/users/:id' do
+end
+
+# delete a user
+delete '/users/:id' do
+end
+
+
